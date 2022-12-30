f5119a286634ed2db9fc1cfddc04e2f6c40cdc95
flooijdt <flooijdt@localhost.localdomain>
2022-12-30 08:56:23 -0300

    message

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -5,4 +5,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod crowdfunding {
     use super::*;
+
+    /* Context is a list of the accounts that the program will interact with */
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {}
 }
