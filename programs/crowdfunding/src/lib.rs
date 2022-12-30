b06a1a2ed5871d80c1eb597d5d760c0541a15b64
flooijdt <flooijdt@localhost.localdomain>
2022-12-30 09:09:54 -0300

    message

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -6,7 +6,8 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod crowdfunding {
     use super::*;
 
-    /* Context is a list of the accounts that the program will interact with */
+    /* Context is a list of the accounts that hold the data that will be used by the program.
+    Remember that programs cannot hold data!  */
     pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
         let campaign = &mut ctx.accounts.campaign;
         campaign.name = name;
