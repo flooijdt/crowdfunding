74f83c8f8f363f4b1cd985e5f1cf109fe207202a
flooijdt <flooijdt@localhost.localdomain>
2022-12-30 09:03:12 -0300

    message

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -7,5 +7,12 @@ pub mod crowdfunding {
     use super::*;
 
     /* Context is a list of the accounts that the program will interact with */
-    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {}
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
+        Ok(())
+    }
 }
