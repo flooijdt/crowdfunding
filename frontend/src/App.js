db63c1ce8aa5a82fa458cc2ce50c15a5e52e1a24
flooijdt <flooijdt@localhost.localdomain>
2022-12-31 17:35:42 -0300

    message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
 const App = () => {
-
+  const checkIfWalletIsConnected = async () => {
+    try {
+      /* Phantom wallet, when connected adds this solana object into your window object */
+      /* Retrieves this object */
+      const solana
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
 
 export default App;
