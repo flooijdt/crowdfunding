6fea9ed22d3bf6d9b6cfab76dec3434a5c8e6b3c
flooijdt <flooijdt@localhost.localdomain>
2022-12-31 08:59:53 -0300

    message

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("D8b9RSTCavUmSMaPcrKTmtXS2qwBxbL45HYCfD3JTYpf");
 
 #[program]
 pub mod crowdfunding {
