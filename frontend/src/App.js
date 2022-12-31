5bb1979112c05a34db779bb0eaf99acbebeb23d1
flooijdt <flooijdt@localhost.localdomain>
2022-12-31 17:49:18 -0300

    message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
+import { useEffect } from "react";
 
 const App = () => {
   const checkIfWalletIsConnected = async () => {
     try {
       /* Phantom wallet, when connected adds this solana object into your window object */
       /* Retrieves this object */
-      const solana
+      const { solana } = window;
+      /* Checks if object exists */
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+        }
+      } else {
+        alert("Solana object not found!");
+      }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, [])
 }
 
 export default App;
